86547ffe2450bb2e4b5d732e39352b6f01c6565b
ihsanGhoziZulfikar <100748074+ihsanGhoziZulfikar@users.noreply.github.com>
2023-02-15 10:44:18 +0700

    Add files via upload

---
+++ Human.java
@@ -0,0 +1,28 @@
+public class Human
+{
+	protected String nik;
+	protected String nama;
+	protected char jenis_kelamin;
+	
+	public Human(){
+	}
+	
+	public String getNik(){
+		return this.nik;
+	}
+	public void setNik(String nik){
+		this.nik = nik;
+	}
+	public String getNama(){
+		return this.nama;
+	}
+	public void setNama(String nama){
+		this.nama = nama;
+	}
+	public char getJenis_kelamin(){
+		return this.jenis_kelamin;
+	}
+	public void setJenis_kelamin(char jenis_kelamin){
+		this.jenis_kelamin = jenis_kelamin;
+	}
+}
